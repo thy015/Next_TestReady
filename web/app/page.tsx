9edc28d387b6ece38c5c9ac6733301306be15be6
Dorothy Tran <thymai.1510@gmail.com>
2025-05-09 23:53:51 +0700

    chore: fixing 1st page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,45 +3,39 @@ import { CollectionList } from '@/components/pages/tests/CollectionList'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
 export default function Home() {
   return (
     // Image background with text and button
     <div>
-      <div className="relative w-full h-[64]">
-        <Image
-          src="/images/study.jpg"
-          alt="Background"
-          width={5621}
-          height={3748}
-          className="w-full h-[320px] sm:h-64 object-cover absolute"
-        />
-
-        <div className="relative z-10 container mx-auto h-full p-4">
-          <div className="grid grid-cols-4 h-full items-center">
-            <div className="col-span-3 lg:col-span-2 text-2xl border-2 border-paleorange border-dashed p-4">
-              <div>Muốn biết khả năng</div>
-              <strong>TOEIC của bạn đến đâu?</strong>
-              <div className="mt-4">
-                <div>
-                  Thử sức{' '}
-                  <span className="text-dark-cyan font-bold">miễn phí</span>,
-                  biết điểm{' '}
-                  <span className="text-dark-cyan font-bold"> tức thì !</span>
-                </div>
-              </div>
-              <Button variant={'paleorange'} className="mt-6 w-40 text-lg h-12">
-                <Link href="/">Bắt đầu ngay</Link>
+      <div className="relative w-full h-[320px]">
+        <div className="w-full grid grid-cols-2 h-full">
+          {/* left side */}
+          <div className="col-span-1 flex flex-col items-start justify-center h-full">
+            <div className="text-lg bg-light-blue text-white w-42 rounded-2xl flex justify-center">
+              TOEIC Preparation
+            </div>
+            <div>
+              <div>Nâng Cao Khả Năng Tiếng Anh</div>
+              <div>Của Bạn Với TOEIC READY</div>
+            </div>
+            <div>
+              Chuẩn bị toàn diện cho bài kiểm tra Tiếng Anh quan trọng. Hãy tham
+              gia làm bài kiểm tra miễn phí và bắt đầu hành trình của bạn ngay
+              hôm nay.
+            </div>
+            <div className="flex justify-center items-center">
+              <Button variant={'paleorange'}>
+                <Link href="/"> Bắt đầu ngay</Link>
+                <ArrowRight></ArrowRight>
+              </Button>
+              <Button>
+                <Link href="/courses">Khám phá khóa học</Link>
               </Button>
             </div>
-            <div className="hidden lg:flex col-span-2">
-              <Image
-                src="/images/readingbook.svg"
-                alt="Logo"
-                width={710}
-                height={789}
-                className="w-64 h-44"
-              />
-            </div>
+          </div>
+          <div className="col-span-1">
+            <div>hi</div>
           </div>
         </div>
       </div>
